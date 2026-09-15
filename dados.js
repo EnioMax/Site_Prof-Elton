74515ece78c8b19597e96e447deb2cf9bce69fb5
EnioMax <eniomax@gmail.com>
2026-09-15 13:07:44 -0300

    Auto: publicação via n8n

--- dados.js
+++ dados.js
@@ -45,5 +45,15 @@ const VIDEOS_YOUTUBE = [
 
 // Matérias jornalísticas - Publicações na imprensa
 const MATERIAS_IMPRENSA = [
+    {
+        tipo: "materia",
+        tag: "Imprensa",
+        titulo: "",
+        resumo: "Análise do Prof. Dr. Elton Gomes publicada na imprensa.",
+        veiculo: "Imprensa",
+        link: "",
+        automatico: true,
+    },
+
 
     ];
