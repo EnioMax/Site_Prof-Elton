ecf0f85bca179f9d2d5b73004ca0512405885ce9
EnioMax <eniomax@gmail.com>
2026-09-15 12:57:52 -0300

    Reverter card vazio adicionado por acidente

--- dados.js
+++ dados.js
@@ -36,14 +36,5 @@ const VIDEOS_YOUTUBE = [
 
 // Matérias jornalísticas - Publicações na imprensa
 const MATERIAS_IMPRENSA = [
-    {
-        tipo: "materia",
-        tag: "Imprensa",
-        titulo: "",
-        resumo: "Análise do Prof. Dr. Elton Gomes publicada na imprensa.",
-        veiculo: "Imprensa",
-        link: "",
-        automatico: true,
-    },
 
     ];
