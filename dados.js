47a8b62e88c812806496242f86a9a0bd45b158ab
EnioMax <eniomax@gmail.com>
2026-09-15 10:31:57 -0300

    Remove test entry from dados.js

--- dados.js
+++ dados.js
@@ -36,45 +36,4 @@ const VIDEOS_YOUTUBE = [
 
 // Matérias jornalísticas - Publicações na imprensa
 const MATERIAS_IMPRENSA = [
-    {
-        tipo: "materia",
-        tag: "Gazeta do Povo",
-        titulo: "Teste validação WF3 - publicar fluxo completo",
-        resumo: "Análise do Prof. Dr. Elton Gomes publicada na imprensa.",
-        veiculo: "Gazeta do Povo",
-        link: "https://www.gazetadopovo.com.br/republica/teste-wf3-publicar/",
-        imagem: "https://media.gazetadopovo.com.br/2019/12/17112031/meta-image-gazeta-do-povo-new.png",
-        automatico: true,
-    },
-
-    {
-        tipo: "materia",
-        tag: "Gazeta do Povo",
-        titulo: "Pesquisa Quaest mostra como está a disputa para presidente",
-        resumo: "Análise do Prof. Dr. Elton Gomes sobre pesquisa eleitoral e cenário político para as eleições de 2026.",
-        veiculo: "Gazeta do Povo",
-        link: "https://www.gazetadopovo.com.br/eleicoes/2026/pesquisa-eleitoral-2026/quaest-pesquisa-presidente-agosto-2026/",
-        imagem: "https://media.gazetadopovo.com.br/2026/08/11172001/pesquisa-presidente-3.png",
-        automatico: true
-    },
-    {
-        tipo: "materia",
-        tag: "Gazeta do Povo",
-        titulo: "Moraes autoriza X a retomar funcionamento no Brasil após multa",
-        resumo: "Análise do Prof. Dr. Elton Gomes sobre decisão judicial e liberdade de expressão no Brasil.",
-        veiculo: "Gazeta do Povo",
-        link: "https://www.gazetadopovo.com.br/republica/moraes-autoriza-x-a-retomar-funcionamento-no-brasil-apos-multa/",
-        imagem: "https://media.gazetadopovo.com.br/2019/12/17112031/meta-image-gazeta-do-povo-new.png",
-        automatico: true
-    },
-    {
-        tipo: "materia",
-        tag: "Gazeta do Povo",
-        titulo: "Relação do PT com Foro de SP engaja oposição e pode pautar eleições de 2026",
-        resumo: "Análise do Prof. Dr. Elton Gomes sobre alianças políticas e o impacto do Foro de São Paulo nas eleições.",
-        veiculo: "Gazeta do Povo",
-        link: "https://www.gazetadopovo.com.br/republica/relacao-do-pt-com-foro-de-sp-engaja-oposicao-e-pode-pautar-eleicoes-de-2026/",
-        imagem: "https://media.gazetadopovo.com.br/2026/01/05142435/52936060313_a7962d4de1_o-scaled.jpg",
-        automatico: true
-    }
-];
+    ];
