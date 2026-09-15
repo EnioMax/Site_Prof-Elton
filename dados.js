1e2354bddbebf52cf17117b6bc222efdfc5a2915
EnioMax <eniomax@gmail.com>
2026-09-15 13:16:12 -0300

    Remover card vazio em MATERIAS_IMPRENSA

--- dados.js
+++ dados.js
@@ -45,15 +45,6 @@ const VIDEOS_YOUTUBE = [
 
 // Matérias jornalísticas - Publicações na imprensa
 const MATERIAS_IMPRENSA = [
-    {
-        tipo: "materia",
-        tag: "Imprensa",
-        titulo: "",
-        resumo: "Análise do Prof. Dr. Elton Gomes publicada na imprensa.",
-        veiculo: "Imprensa",
-        link: "",
-        automatico: true,
-    },
 
 
     ];
