c4675c4f620a89506f096bfaecea446aa2af9842
EnioMax <eniomax@gmail.com>
2026-09-16 11:38:51 -0300

    Update dados.js (pipeline)

--- dados.js
+++ dados.js
@@ -52,6 +52,7 @@ const VIDEOS_YOUTUBE = [
 // Matérias jornalísticas - Publicações na imprensa
 // Matérias jornalísticas - Publicações na imprensa
 // Matérias jornalísticas - Publicações na imprensa
+// Matérias jornalísticas - Publicações na imprensa
 const MATERIAS_IMPRENSA = [
     {
         "titulo": "Pesquisa Quaest mostra como está a disputa para presidente",
