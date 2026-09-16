c41c0c2639d5ae157951014bc9fdc9f3314620d1
EnioMax <eniomax@gmail.com>
2026-09-16 16:41:50 -0300

    Update dados.js (pipeline)

--- dados.js
+++ dados.js
@@ -50,7 +50,8 @@ const MATERIAS_IMPRENSA = [
         "veiculo": "Gazeta do Povo",
         "link": "https://www.gazetadopovo.com.br/eleicoes/2026/pesquisa-eleitoral-2026/quaest-pesquisa-presidente-agosto-2026/",
         "tag": "📰 Gazeta do Povo",
-        "resumo": ""
+        "resumo": "",
+        "imagem": "https://media.gazetadopovo.com.br/2026/08/11172001/pesquisa-presidente-3.png"
     },
     {
         "titulo": "Teste publicação via n8n - matéria de validação",
@@ -64,62 +65,71 @@ const MATERIAS_IMPRENSA = [
         "veiculo": "Gazeta do Povo",
         "link": "https://www.gazetadopovo.com.br/republica/moraes-autoriza-x-a-retomar-funcionamento-no-brasil-apos-multa/",
         "tag": "📰 Gazeta do Povo",
-        "resumo": ""
+        "resumo": "",
+        "imagem": "https://media.gazetadopovo.com.br/2019/12/17112031/meta-image-gazeta-do-povo-new.png"
     },
     {
         "titulo": "Relação do PT com Foro de SP engaja oposição e pode pautar eleições de 2026",
         "veiculo": "Gazeta do Povo",
         "link": "https://www.gazetadopovo.com.br/republica/relacao-do-pt-com-foro-de-sp-engaja-oposicao-e-pode-pautar-eleicoes-de-2026/",
         "tag": "📰 Gazeta do Povo",
-        "resumo": ""
+        "resumo": "",
+        "imagem": "https://media.gazetadopovo.com.br/2026/01/05142435/52936060313_a7962d4de1_o-scaled.jpg"
     },
     {
         "titulo": "Candidatura de Joaquim Barbosa nasce minada pela polarização",
         "veiculo": "Gazeta do Povo",
         "link": "https://www.gazetadopovo.com.br/eleicoes/2026/candidatura-de-joaquim-barbosa-ja-nasce-cercada-de-descrenca-e-obstaculos/",
         "tag": "📰 Gazeta do Povo",
-        "resumo": ""
+        "resumo": "",
+        "imagem": "https://media.gazetadopovo.com.br/2022/01/11120451/joaquim-barbosa-crop-20220111150435.jpg"
     },
     {
         "titulo": "Boulos e João Campos ainda não animam a esquerda como futuros líderes do pós-Lula",
         "veiculo": "Gazeta do Povo",
         "link": "https://www.gazetadopovo.com.br/republica/boulos-e-joao-campos-ainda-nao-animam-a-esquerda-como-futuros-lideres-do-pos-lula/",
         "tag": "📰 Gazeta do Povo",
-        "resumo": ""
+        "resumo": "",
+        "imagem": "https://media.gazetadopovo.com.br/2023/04/24220940/img20230412200844923MED.jpg"
     },
     {
         "titulo": "Candidatura de Joaquim Barbosa nasce minada pela polarização",
         "veiculo": "Gazeta do Povo",
         "link": "https://www.gazetadopovo.com.br/eleicoes/2026/candidatura-de-joaquim-barbosa-ja-nasce-cercada-de-descrenca-e-obstaculos/",
         "tag": "📰 Gazeta do Povo",
-        "resumo": ""
+        "resumo": "",
+        "imagem": "https://media.gazetadopovo.com.br/2022/01/11120451/joaquim-barbosa-crop-20220111150435.jpg"
     },
     {
         "titulo": "Ciência Política: origens e formação",
         "veiculo": "YouTube",
         "link": "https://www.youtube.com/watch?v=saYf8xp2Lb0",
         "tag": "📰 YouTube",
-        "resumo": ""
+        "resumo": "",
+        "imagem": "https://i.ytimg.com/vi/saYf8xp2Lb0/maxres2.jpg?sqp=-oaymwEoCIAKENAF8quKqQMcGADwAQH4Ac4FgAKACooCDAgAEAEYZSBTKEUwDw==&amp;rs=AOn4CLDo5bPhNP82XUn11I4t4lkZVT294Q"
     },
     {
         "titulo": "Programa Tardiar 05/07 — A Tempestade no Oriente Médio: O Impacto Global do Conflito Israel-Irã",
         "veiculo": "YouTube",
         "link": "https://www.youtube.com/watch?v=LDFBiOECaNY",
         "tag": "📰 YouTube",
-        "resumo": ""
+        "resumo": "",
+        "imagem": "https://i.ytimg.com/vi/LDFBiOECaNY/hqdefault.jpg"
     },
     {
         "titulo": "Programa Tardiar 17/05 — BRICS vai mudar o mundo? Dr. Elton Gomes analisa o futuro do bloco",
         "veiculo": "YouTube",
         "link": "https://www.youtube.com/watch?v=ceQbAwpizO8",
         "tag": "📰 YouTube",
-        "resumo": ""
+        "resumo": "",
+        "imagem": "https://i.ytimg.com/vi/ceQbAwpizO8/maxresdefault.jpg"
     },
     {
         "titulo": "Programa Tardiar 17/05 — BRICS vai mudar o mundo? Dr. Elton Gomes analisa o futuro do bloco",
         "veiculo": "YouTube",
         "link": "https://www.youtube.com/watch?v=91OQ6FXVmjU",
         "tag": "📰 YouTube",
-        "resumo": ""
+        "resumo": "",
+        "imagem": "https://i.ytimg.com/vi/91OQ6FXVmjU/hqdefault.jpg"
     }
 ];
